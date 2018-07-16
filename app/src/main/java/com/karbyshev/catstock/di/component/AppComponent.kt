0ca0a802248aa6a028dfe6db7e017cddf5abb455
Karbyshev Den <karbyshev.den@gmail.com>
2018-07-16 09:26:01 +0300

    Dependency injections

--- app/src/main/java/com/karbyshev/catstock/di/component/AppComponent.kt
+++ app/src/main/java/com/karbyshev/catstock/di/component/AppComponent.kt
@@ -1,4 +1,17 @@
 package com.karbyshev.catstock.di.component
 
+
+import com.karbyshev.catstock.di.module.ItemDaoModule
+import com.karbyshev.catstock.mvp.presenter.MainPresenter
+import com.karbyshev.catstock.mvp.presenter.NotePresenter
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [ItemDaoModule::class])
 interface AppComponent {
+
+    fun inject(mainPresenter: MainPresenter)
+
+    fun inject(notePresenter: NotePresenter)
 }
--- app/src/main/java/com/karbyshev/catstock/di/module/ItemDaoModule.kt
+++ app/src/main/java/com/karbyshev/catstock/di/module/ItemDaoModule.kt
@@ -1,2 +1,14 @@
 package com.karbyshev.catstock.di.module
 
+import com.karbyshev.catstock.mvp.model.ItemDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ItemDaoModule {
+
+    @Provides
+    @Singleton
+    fun provideItemDao(): ItemDao = ItemDao()
+}
