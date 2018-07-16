4a9f98480a212c28fd0b084821f58394f92bdb90
Karbyshev Den <karbyshev.den@gmail.com>
2018-07-16 09:27:40 +0300

    Application

--- app/src/main/java/com/karbyshev/catstock/App.kt
+++ app/src/main/java/com/karbyshev/catstock/App.kt
@@ -1,10 +1,38 @@
 package com.karbyshev.catstock
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
+import com.karbyshev.catstock.di.component.AppComponent
+import com.karbyshev.catstock.di.component.DaggerAppComponent
+import com.karbyshev.catstock.di.module.ItemDaoModule
+import com.karbyshev.catstock.mvp.model.AppDatabase
+import com.karbyshev.catstock.mvp.model.Item
+import com.reactiveandroid.ReActiveAndroid
+import com.reactiveandroid.ReActiveConfig
+import com.reactiveandroid.internal.database.DatabaseConfig
 
 class App: Application() {
 
+    companion object {
+        lateinit var graph: AppComponent
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
     override fun onCreate() {
         super.onCreate()
+
+        context = this
+
+        graph = DaggerAppComponent.builder().itemDaoModule(ItemDaoModule()).build()
+
+        val appDatabaseConfig = DatabaseConfig.Builder(AppDatabase::class.java)
+                .addModelClasses(Item::class.java)
+                .build()
+
+        ReActiveAndroid.init(ReActiveConfig.Builder(this)
+                .addDatabaseConfigs(appDatabaseConfig)
+                .build())
     }
 }
