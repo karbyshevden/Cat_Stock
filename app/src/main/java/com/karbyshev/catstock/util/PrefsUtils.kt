df3c97c6486856a2a0e7c37c534c9c7cdfc60190
Karbyshev Den <karbyshev.den@gmail.com>
2018-07-16 09:28:43 +0300

    preferences

--- app/src/main/java/com/karbyshev/catstock/util/PrefsUtils.kt
+++ app/src/main/java/com/karbyshev/catstock/util/PrefsUtils.kt
@@ -1,2 +1,14 @@
 package com.karbyshev.catstock.util
 
+import android.content.Context
+import com.karbyshev.catstock.App
+
+private val prefs by lazy {
+    App.context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+}
+
+fun getNotesSortMethodName(defaultValue: String): String = prefs.getString("sort_method", defaultValue)
+
+fun setNotesSortMethod(sortMethod: String) {
+    prefs.edit().putString("sort_method", sortMethod).apply()
+}
